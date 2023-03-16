d61470a492d971ee9038818ea3d9da51ee2c46d4
Spatium-Lustrator <72788088+Spatium-Lustrator@users.noreply.github.com>
2023-03-16 22:20:52 +0300

    Add files via upload

--- routes/user_account_routes.js
+++ routes/user_account_routes.js
@@ -2,7 +2,7 @@ const Router = require("express")
 const router = new Router()
 const userAccountController = require('../controllers/user_account_controller')
 
-router.post("/user-account-save", userAccountController.save_user_account)
-router.delete("/user-account-delete", userAccountController.delete_user_account)
+router.post("/user-account-save", userAccountController.save_user_info)
+router.delete("/user-account-delete", userAccountController.delete_user_info)
 
 module.exports = router
