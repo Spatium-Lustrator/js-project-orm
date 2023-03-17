9cb8e6ff51c84141ec08a3c63e94afb73912c368
Spatium-Lustrator <72788088+Spatium-Lustrator@users.noreply.github.com>
2023-03-17 17:17:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require("express")
 const sequelize = require('./db')
 
 // db models
-// const university_model = require('./models/university_model')
-// const admin_model = require('./models/admin_model')
-// const certificate_id_model = require('./models/certificate_id_model')
-// const user_model = require('./models/user_model')
-// const user_info_model = require('./models/user_info_model')
-
 const user_account_model = require('./models/user_acount_model')
+const federal_district_model = require('./models/federal_district_model')
+const region_model = require('./models/region_model')
+const university_in_region_model = require('./models/university_in_region_model')
+const university_model = require('./models/university_model')
+const user_in_university_model = require('./models/user_in_university_model')
+const user_model = require('./models/user_model')
 
 // routers
-// const university_router = require('./routes/universtiy_routes')
-// const user_info_router = require('./routes/user_info_routes')
 const user_account_router = require('./routes/user_account_routes')
 
 const app = express()
@@ -20,8 +18,6 @@ const PORT = "5000"
 
 app.use(express.json())
 
-// app.use('/university', university_router, (req, res) => {})
-// app.use('/user-info', user_info_router, (req, res) => {})
 app.use('/user-account', user_account_router, (req, res) => {})
 
 const start = async () => {
@@ -36,4 +32,4 @@ const start = async () => {
 }
 
 
-start();
+start(); 
