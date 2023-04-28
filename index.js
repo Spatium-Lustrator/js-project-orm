34ec133b995ac95d352a4c6baa54c976ae66af58
Spatium-Lustrator <72788088+Spatium-Lustrator@users.noreply.github.com>
2023-04-28 16:59:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const user_model = require('./models/user_model')
 
 // routers
 const user_account_router = require('./routes/user_account_routes')
+const federal_district_router = require('./routes/federal_district_routes')
+const university_router = require('./routes/universtiy_routes')
+const user_router = require('./routes/user_routes')
+const user_in_university = require('./routes/user_in_university_routes')
+const region = require("./routes/region_routes")
+const university_in_region = require("./routes/university_in_region_routes")
 
 const app = express()
 const PORT = "5000"
@@ -19,6 +25,12 @@ const PORT = "5000"
 app.use(express.json())
 
 app.use('/user-account', user_account_router, (req, res) => {})
+app.use('/federal-district', federal_district_router, (req, res) => {})
+app.use('/user', user_router, (req, res) => {})
+app.use('/university', university_router, (req, res) => {})
+app.use('/user-in-university', user_in_university, (req, res) => {})
+app.use('/region', region, (req, res) => {})
+app.use('/university-in-region', university_in_region, (req, res) => {})
 
 const start = async () => {
     try {
